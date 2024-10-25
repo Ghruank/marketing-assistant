1068fb035ded169a456d395f1e8fe331d1ca866c
Kartikay-0111 <kartikay7905@gmail.com>
2024-10-26 04:27:56 +0530

    lnding

--- app/src/App.js
+++ app/src/App.js
@@ -1,36 +1,11 @@
 // App.jsx
-import React, { useState } from "react";
-
+import React from "react";
+import Home from "./components/Home";
 function App() {
-  const [prompt, setPrompt] = useState("");
-  const [response, setResponse] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const res = await fetch("http://127.0.0.1:5000/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ prompt }),
-    });
-    const data = await res.json();
-    console.log(data);
-    setResponse(data.message); // Display the response from MetaAI
-  };
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          placeholder="Enter prompt"
-        />
-        <button type="submit">Send</button>
-      </form>
-      <div>{response}</div>
+      <Home />
     </div>
   );
 }
