3247376085a021df7341aa554964b1aa75c23f82
Ghruank <ghruankkothare@gmail.com>
2024-10-26 08:54:02 +0530

    arre yaar

--- app/src/App.js
+++ app/src/App.js
@@ -4,25 +4,20 @@ import PromptInputBar from './components/PromptInputBar';
 // import Home from './components/Home';
 // import InteractiveBackground from './components/interactivebg';
 import { Bg } from './components/bg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import InteractiveBackground from './components/interactivebg';
 
 function App() {
 
-  // const handleSubmit = async (e) => {
-  //     e.preventDefault();
-  //     const res = await fetch("http://127.0.0.1:5000/", {
-  //         method: "POST",
-  //         headers: { "Content-Type": "application/json" },
-  //         body: JSON.stringify({ prompt }),
-  //     });
-  //     const data = await res.json();
-  //     console.log(data);
-  //     setResponse(data.message);  
-  // };
-
   return (
     <div >
-      <Bg />
-      {/* <PromptInputBar /> */}
+        <Router>
+            <Routes>
+                <Route path="/" element={<Bg />} />
+                <Route path="/main" element={<PromptInputBar/>} />
+            </Routes>
+        </Router>
     </div>
   );
 }
--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // import { a } from 'react-router-dom';
 import styled from "styled-components";
+import { Github } from 'lucide-react';
+import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className=" py-4 pl-9 pr-9">
@@ -24,13 +26,13 @@ function Navbar() {
               />
             </svg>
           </div>
-          <span className="text-white text-xl font-bold">ZaverAI</span>
+          <span className="text-white text-xl font-bold">Mappa</span>
         </div>
 
         {/* Right side: Nav links and icons */}
         <div className="flex items-center space-x-6 text-white mr-9 rounded-full pl-6 pr-6 pt-2 pb-2 bg-transparent border-2 border-sky-300">
           <a href="#" className="hover:text-cyan-400 transition duration-300">
-            Get started
+            About Project
           </a>
           <a href="#" className="hover:text-cyan-400 transition duration-300">
             Plugins
@@ -43,8 +45,8 @@ function Navbar() {
 
           {/* Install Button */}
           <a href="#" className="ml-6">
-            <button className="bg-cyan-500 text-black px-4 py-2 rounded-full hover:bg-cyan-600 transition duration-300 ">
-              INSTALL
+            <button className="bg-cyan-500 text-black px-4 py-2 rounded-full hover:bg-cyan-600 transition duration-300 flex ">
+            <Github /> <a href="https://github.com/Ghruank/marketing-assistant">GitHub</a>
             </button>
           </a>
         </div>
